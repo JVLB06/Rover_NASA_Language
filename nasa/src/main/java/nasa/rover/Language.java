e233e6214aa4f945cd559f17060e47cfc72d3e80
João Butzen <butzenjvlb@gmail.com>
2026-05-05 20:25:33 -0300

    feat/ Add print parts

--- nasa/src/main/java/nasa/rover/Language.java
+++ nasa/src/main/java/nasa/rover/Language.java
@@ -13,6 +13,31 @@ public class Language {
     public int[][] matrixUsedPath = {{}};
     public String phases = "";
     public Scanner input = new Scanner(System.in);
-    
+    public String commands = "";
+
+    public String getCommands() {
+        return commands;
+    }
+    public void setCommands(String commands) {
+        this.commands = commands;
+    }
+    public int[][] getMatrixUsedPath() {
+        return matrixUsedPath;
+    }
+    public void setMatrixUsedPath(int[][] matrixUsedPath) {
+        this.matrixUsedPath = matrixUsedPath;
+    }
+    public String getPhases() {
+        return phases;
+    }
+    public void setPhases(String phases) {
+        this.phases = phases;
+    }
+    public Scanner getInput() {
+        return input;
+    }
+    public void setInput(Scanner input) {
+        this.input = input;
+    }
     
 }
--- nasa/src/main/java/nasa/rover/Main.java
+++ nasa/src/main/java/nasa/rover/Main.java
@@ -6,6 +6,7 @@ import nasa.rover.Language;
 public class Main {
     public static void main(String[] args) {
         Language language = new Language();
+
         System.out.println(Consts.openPhrase);
         System.out.println("Hi rover. This is your board:");
         Utils.printMatrix(Consts.matrix);
@@ -13,6 +14,12 @@ public class Main {
         System.out.println("You can move " + Consts.walkLimit + " steps at a time, but you must move at least " + Consts.walkMinimum + " step.");
         System.out.println("You can move in 3 directions: left, right and turn over, and walk straight forward.");
 
-        language.phases = language.input.nextLine();
+        Utils.printMatrix(Consts.matrix);
+
+        language.setPhases(language.getInput().nextLine());
+        language.setCommands(Utils.loadPath(Consts.matrix, language.getPhases()));
+        language.setMatrixUsedPath(Utils.markPath(Consts.matrix, language.getCommands()));
+
+        Utils.printEndMatrix(language.getMatrixUsedPath());
     }
 }
